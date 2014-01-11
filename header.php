5d57152ed82b16ccaf843c91abf8701bb2db3ed4
boluge <boluge@gmail.com>
2014-01-11 14:31:38 +0100

    Compatible IE8 et IE7

--- header.php
+++ header.php
@@ -12,6 +12,13 @@
 		<meta name="viewport" content="width=device-width,initial-scale=1.0">
 		<meta name="description" content="<?php bloginfo('description'); ?>">
 
+	<!--[if lt IE 9]>
+		<script src="//cdnjs.cloudflare.com/ajax/libs/html5shiv/3.6.2/html5shiv.js"></script>
+		<script src="//s3.amazonaws.com/nwapi/nwmatcher/nwmatcher-1.2.5-min.js"></script>
+		<script src="//html5base.googlecode.com/svn-history/r38/trunk/js/selectivizr-1.0.3b.js"></script>
+		<script src="//cdnjs.cloudflare.com/ajax/libs/respond.js/1.1.0/respond.min.js"></script>
+	<![endif]-->
+
 		<?php wp_head(); ?>
 		<script>
         // conditionizr.com
