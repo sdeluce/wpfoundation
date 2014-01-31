909fe306e52ce9ee46b4225346c4bd9fcd0be871
Stéphane <boluge@gmail.com>
2014-01-31 16:23:34 +0100

    Kill meta description

--- header.php
+++ header.php
@@ -8,7 +8,6 @@
 
 		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 		<meta name="viewport" content="width=device-width,initial-scale=1.0">
-		<meta name="description" content="<?php bloginfo('description'); ?>">
 
 	<!--[if lt IE 9]>
 		<script src="//cdnjs.cloudflare.com/ajax/libs/html5shiv/3.6.2/html5shiv.js"></script>
