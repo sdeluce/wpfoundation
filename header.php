e4ddf8229c7bc6797c0764a556d355ef2b2999dc
boluge <boluge@gmail.com>
2014-02-01 20:20:16 +0100

    up ie

--- header.php
+++ header.php
@@ -9,21 +9,24 @@
 		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 		<meta name="viewport" content="width=device-width,initial-scale=1.0">
 
-	<!--[if lt IE 9]>
+		<?php wp_head(); ?>
+
+	<!--[if lt IE 9]>		
 		<script src="//cdnjs.cloudflare.com/ajax/libs/html5shiv/3.6.2/html5shiv.js"></script>
 		<script src="//s3.amazonaws.com/nwapi/nwmatcher/nwmatcher-1.2.5-min.js"></script>
 		<script src="//html5base.googlecode.com/svn-history/r38/trunk/js/selectivizr-1.0.3b.js"></script>
 		<script src="//cdnjs.cloudflare.com/ajax/libs/respond.js/1.1.0/respond.min.js"></script>
+		<script src="<?php echo get_template_directory_uri(); ?>/js/rem.min.js"></script>
+		<link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri(); ?>/css/grid_ie.css">
 	<![endif]-->
-
-		<?php wp_head(); ?>
+		
 		<script>
-        // conditionizr.com
-        conditionizr.config({
-            assets: '<?php echo get_template_directory_uri(); ?>',
-            tests: {}
-        });
-        </script>
+		// conditionizr.com
+		conditionizr.config({
+			assets: '<?php echo get_template_directory_uri(); ?>',
+			tests: {}
+		});
+		</script>
 
 	</head>
 	<body <?php body_class('antialiased'); ?>>
